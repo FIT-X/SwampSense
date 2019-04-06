e32d43d6e6dd68f48532f6775afb13c1a20be4bf
Chris Woodle <chris@droplit.io>
2019-04-06 13:10:18 -0400

    adjusted whitespace

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <button @click='ledOn'>On</button>
-    <button @click='ledOff'>Off</button>
-  </div>
+    <div class="home">
+        <button @click='ledOn'>On</button>
+        <button @click='ledOff'>Off</button>
+    </div>
 </template>
 
 <script lang="ts">
@@ -12,47 +12,47 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import axios from "axios";
 
 @Component({
-  components: {
-    HelloWorld
-  }
+    components: {
+        HelloWorld
+    }
 })
 export default class Home extends Vue {
-  mounted() {
-    console.log(process.env.VUE_APP_PARTICLE_ACCESS_TOKEN);
-  }
+    mounted() {
+        console.log(process.env.VUE_APP_PARTICLE_ACCESS_TOKEN);
+    }
 
-  ledOn() {
-    const url = `https://api.particle.io/v1/devices/34003e000d47373334323233/led?access_token=${
-      process.env.VUE_APP_PARTICLE_ACCESS_TOKEN
-    }`;
-    axios
-      .post(url, {
-        args: "on"
-      })
-      .then((response) => {
-        console.log(response);
-        this.$toasted.show(response.data.return_value)
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+    ledOn() {
+        const url = `https://api.particle.io/v1/devices/34003e000d47373334323233/led?access_token=${
+            process.env.VUE_APP_PARTICLE_ACCESS_TOKEN
+            }`;
+        axios
+            .post(url, {
+                args: "on"
+            })
+            .then((response) => {
+                console.log(response);
+                this.$toasted.show(response.data.return_value)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-  ledOff() {
-    const url = `https://api.particle.io/v1/devices/34003e000d47373334323233/led?access_token=${
-      process.env.VUE_APP_PARTICLE_ACCESS_TOKEN
-    }`;
-    axios
-      .post(url, {
-        args: "off"
-      })
-      .then((response) => {
-        this.$toasted.show(response.data.return_value)
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+    ledOff() {
+        const url = `https://api.particle.io/v1/devices/34003e000d47373334323233/led?access_token=${
+            process.env.VUE_APP_PARTICLE_ACCESS_TOKEN
+            }`;
+        axios
+            .post(url, {
+                args: "off"
+            })
+            .then((response) => {
+                this.$toasted.show(response.data.return_value)
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 }
 </script>
