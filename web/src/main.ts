65930bdfc364adb45b04ea117d3368fb21043727
Chris Woodle <chris@droplit.io>
2019-04-06 13:16:55 -0400

    added thermometer

--- web/src/main.ts
+++ web/src/main.ts
@@ -6,8 +6,13 @@ import './style.scss'
 
 Vue.config.productionTip = false
 
+// 3rd Party Libraries
 import Toasted from 'vue-toasted';
 Vue.use(Toasted);
+
+// @ts-ignore
+import VueThermometer from 'vuejs-thermometer';
+Vue.use(VueThermometer)
 
 new Vue({
   router,
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="home">
+        <vue-thermometer
+            :value="temperature"
+            :min="0"
+            :max="255" />
         <button @click='ledOn'>On</button>
         <button @click='ledOff'>Off</button>
     </div>
@@ -11,14 +15,34 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 import axios from "axios";
 
+const POLL_RATE = 5000;
+
 @Component({
     components: {
         HelloWorld
     }
 })
 export default class Home extends Vue {
+    temperature = 0;
+
     mounted() {
         console.log(process.env.VUE_APP_PARTICLE_ACCESS_TOKEN);
+        setInterval(() => {
+            const url = `https://api.particle.io/v1/devices/34003e000d47373334323233/temp?access_token=${
+                process.env.VUE_APP_PARTICLE_ACCESS_TOKEN
+                }`;
+            axios
+                .post(url, {
+                    args: "air"
+                })
+                .then((response) => {
+                    console.log(response);
+                    this.temperature = response.data.return_value;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }, POLL_RATE)
     }
 
     ledOn() {
